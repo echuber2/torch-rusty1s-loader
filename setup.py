6e67a5567d10a3cc61613361a398661e21935197
Eric Huber <echuber2@illinois.edu>
2020-08-16 07:07:41 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+# make sure torch is installed
+import os
+os.system('python3 -m pip install torch')
+
+from setuptools import setup
+setup(
+    name='torch-rusty1s-loader',
+    version='0.1',
+    extras_require={
+        "findblas": ["findblas"],
+        "torch-bincount": ["torch-bincount"],
+        "torch-cluster": ["torch-cluster"],
+        "torch-geometric": ["torch-geometric"],
+        "torch-scatter": ["torch-scatter"],
+        "torch-sparse": ["torch-sparse"],
+        "torch-spline-conv": ["torch-spline"],
+        "torch-unique": ["torch-unique"]
+    }
+)
